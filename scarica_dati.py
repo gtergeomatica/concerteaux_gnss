e93cbe813831d4830d6e76b7e76d6aecf06e4c76
Lorenzo <lorenzo.benvenuto@gter.it>
2020-03-10 14:28:03 +0100

    caricato il primo file

---
+++ scarica_dati.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Copyleft Gter srl 2020
+# Lorenzo Benvenuto
+
+
+
+#passi da fare
+
+#1. controllare che il dato ci sia e nel caso scaricarlo. Se il dato non ci fosse scrivere su un file di log
+
+#1.1 per scaricare il dato bisogna comporre in automatico il suo nome (quindi nomenclatura RINEX 3.02)
+
+
+Stazioni=['XXMG','CAMA','AIGI','BEAN','SAOR']
+
